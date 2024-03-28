860bde8359ae1b6802854dc7ab1731d6305ec0eb
ekam-walia <164983405+ekam-walia@users.noreply.github.com>
2024-03-28 14:09:27 +0530

    Initial commit

---
+++ TICTACTOE/TickityTacTowApp.swift
@@ -0,0 +1,19 @@
+//
+//  TickityTacTowApp.swift
+//  TICTACTOE
+//
+//  Created by Ekam Walia on 30/09/22.
+//
+
+
+
+import SwiftUI
+
+@main
+struct TickityTacTowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
